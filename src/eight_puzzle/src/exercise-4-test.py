092055087a807291008bcfcc7eccdf587defbe86
maykon-marcos-jr <maykon.marcos.junior.dev@gmail.com>
2026-04-07 11:10:18 -0300

    Lesson 4

---
+++ src/eight_puzzle/src/exercise-4-test.py
@@ -0,0 +1,55 @@
+"""Exercício-4: Fluxo de Dados
+
+Considere como unidade de programa o método get_tile da classe PuzzleGame do projeto EightPuzzle.
+
+Para esta unidade:
+1. Faça o GFD (Grafo de Fluxo de Dados).
+2. Indique os caminhos selecionados utilizando os seguintes critérios de teste de fluxo de dados:
+    (i) All-c-uses/Some-p-uses e
+    (ii) All-p-uses.
+    Considere os dados line e column.
+3. Para cada caminho selecionado faça um teste de unidade.
+4. Mostre a execução de cada teste de unidade utilizando o debug para que seja possível mostrar a execução dos comandos do m
+---
+A execução de cada teste de unidade (um teste para cada caminho) utilizando o debug para que seja possível ver os comandos internos do método get_tile que foram executados.
+"""
+
+import unittest
+
+from puzzle_game import PuzzleGame
+from invalid_position_exception import InvalidPositionException
+
+class ClasseTestesAllP(unittest.TestCase):
+    def setUp(self):
+        self.dimension = 3
+        self.game = PuzzleGame(self.dimension)
+
+    def test_line_bellow_0(self):
+        with self.assertRaises(InvalidPositionException):
+            l, c = -1, 1
+            self.game.get_tile(l, c)
+
+    def test_non_empty_line(self):
+        l, c = 2, 3
+        tile = self.game.get_tile(l, c)
+        self.assertEqual(tile, 6)
+
+    def test_empty_position(self):
+        l, c = 3, 3
+        tile = self.game.get_tile(l, c)
+        self.assertEqual(tile, " ")
+
+
+class ClasseTestAllC_SomeP(unittest.TestCase):
+    def setUp(self):
+        self.dimension = 3
+        self.game = PuzzleGame(self.dimension)
+
+    def test_get_valid_tile(self):
+        l, c = 1, 1
+        tile = self.game.get_tile(l, c)
+        self.assertEqual(tile, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
